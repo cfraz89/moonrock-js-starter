9353eaca10481e3958d296037f9c66f84dc94524
FRASER, Chris <Chris.FRASER@suncorp.com.au>
2015-07-15 00:29:51 +1000

    First commit

---
+++ config.js
@@ -0,0 +1,26 @@
+System.config({
+  "transpiler": "traceur",
+  "paths": {
+    "*": "*.js",
+    "github:*": "jspm_packages/github/*.js",
+    "npm:*": "jspm_packages/npm/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "moonrock": "github:cfraz89/moonrock-js@master",
+    "traceur": "github:jmcriffey/bower-traceur@0.0.88",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88",
+    "github:cfraz89/moonrock-js@master": {
+      "rx": "npm:rx@2.5.3"
+    },
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "npm:rx@2.5.3": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    }
+  }
+});
+
